40d5db95b0cd79db7a2950a674ef9bc7c02548bb
Do-heewan <nhw3152@gmail.com>
2025-01-14 14:39:06 +0900

    25.01.14 / Python / 미들러문제

--- python_middler/1654. 랜선 자르기/랜선 자르기.py
+++ python_middler/1654. 랜선 자르기/랜선 자르기.py
@@ -3,10 +3,10 @@
 import sys
 
 input = sys.stdin.readline
-N, K = map(int, input().split())
+K, N = map(int, input().split())
 
-rope = []
-for _ in range(N):
+rope = [] # 랜선의 길이 저장
+for _ in range(K):
     length = int(input())
     rope.append(length)
 
@@ -15,13 +15,13 @@ start, end = 1, max(rope)
 while (start <= end):
     mid = (start + end) // 2
 
-    num_rope = []
+    num_rope = [] # 자른 후의 랜선의 개수 저장
     for i in rope:
         num_rope.append(i // mid)
         
-    if (sum(num_rope) < K):
-        end = mid - 1
-    elif (sum(num_rope) >= K):
-        start = mid + 1
+    if (sum(num_rope) < N): # 랜선의 개수가 필요 랜선의 개수보다 작을 경우
+        end = mid - 1 # mid값을 조정하여 더 잘개 자르도록 조정
+    elif (sum(num_rope) >= N): # 랜선의 개수가 필요 랜선의 개수보다 클 경우
+        start = mid + 1 # mid값을 조정하여 덜 잘개 자르도록 조정
 
-print(end)
+print(end) # start값이 end값보다 커질 때, while문이 종료되고 이때의 end값이 최대 길이가 된다.
