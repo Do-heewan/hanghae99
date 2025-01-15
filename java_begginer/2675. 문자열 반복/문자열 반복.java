130e9ab9b6b23bf657e6660616960e28512c00d4
Do-heewan <nhw3152@gmail.com>
2025-01-15 20:14:56 +0900

    25.01.15 / Java / 비기너문제

---
+++ java_begginer/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int T = s.nextInt();
+        for (int i = 0; i < T; i++) {
+            int R = s.nextInt();
+            String S = s.next();
+
+            for (int j = 0; j < S.length(); j++) {
+                for (int k = 0; k < R; k++) {
+                    System.out.print(S.charAt(j));
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
