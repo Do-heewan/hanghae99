9454c065ac3590e9f1b8eae0b68532ceeae0cd80
Do-heewan <nhw3152@gmail.com>
2025-02-05 16:03:03 +0900

    Day 13 / Python / Middler

--- python_middler/13일차 부등호/부등호.py
+++ python_middler/13일차 부등호/부등호.py
@@ -1,11 +1,15 @@
 # 2529 부등호
 
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 op = list(input().split())
 
-visited = [0] * 10
-result = []
+visited = [False] * 10 # 
+result = [] # 결과 저장 리스트
 
+# 부등호 판별
 def check(a, b, op):
     if (op == "<"):
         if (a > b):
@@ -17,19 +21,18 @@ def check(a, b, op):
     
     return True
 
+# DFS 알고리즘
 def dfs(count, num):
-    if (count == (N+1)):
-        result.append(num)
+    if (count == (N+1)): # 자릿수를 만족하면
+        result.append(num) # 리스트에 추가
         return
     
-    for i in range(10):
-        if (visited[i]):
-            continue
-
-        if (count == 0) or (check(num[-1], str(i), op[count-1])):
-            visited[i] = 1
-            dfs(count+1, num + str(i))
-            visited[i] = 0
+    for i in range(10): # 0 ~ 9까지 탐색
+        if not (visited[i]): # 이미 나온 수는 통과 (중복 존재 x)
+            if (count == 0) or (check(num[-1], str(i), op[count-1])): # 첫번째 자리 or check()가 참일 경우
+                visited[i] = True # 방문 표시
+                dfs(count+1, num + str(i)) # 다음 숫자 재귀
+                visited[i] = False # 방문 표시 해제
 
 dfs(0, '')
 
